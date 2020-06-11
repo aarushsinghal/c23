4a7604c3f749afa9e34b3f6a224ba9b0fc981253
aarushsinghal <66258983+aarushsinghal@users.noreply.github.com>
2020-06-11 19:56:57 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,21 @@
+class Ground {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic:true,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
